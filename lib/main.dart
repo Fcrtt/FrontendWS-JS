8224d2d7806a9c89803a58a9b1ee910ee8f2199a
felix <felix.courtin@gmail.com>
2024-03-04 23:50:28 +0100

    ajout graph

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dashboard/pages/intro_page.dart';
+import 'package:dashboard/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,47 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("HOMEPAGE"),),
+      body: Center(
+        child: MyLineChart(),
+      ),
+    );
+  }
+}
+
+
+class MyLineChart extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return LineChart(
+      LineChartData(
+        gridData: FlGridData(show: false),
+        titlesData: FlTitlesData(show: false),
+        borderData: FlBorderData(
+          show: true,
+          border: Border.all(color: const Color(0xff37434d), width: 1),
+        ),
+        minX: 0,
+        maxX: 7,
+        minY: 0,
+        maxY: 6,
+        lineBarsData: [
+          LineChartBarData(
+            spots: [
+              FlSpot(0, 3),
+              FlSpot(1, 1),
+              FlSpot(2, 4),
+              FlSpot(3, 2),
+              FlSpot(4, 5),
+              FlSpot(5, 1),
+              FlSpot(6, 4),
+            ],
+            isCurved: true,
+
+            dotData: FlDotData(show: true),
+            belowBarData: BarAreaData(show: false),
+          ),
+        ],
+      ),
     );
   }
 }
