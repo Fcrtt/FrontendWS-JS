1355314f2556cd23d8780c45cb246da02cf65534
felix <felix.courtin@gmail.com>
2024-03-06 19:56:24 +0100

    deux containers avec carte

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,19 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_maps/maps.dart';
 
-class IntroPage extends StatelessWidget {
-  const IntroPage({super.key});
+class IntroPage extends StatefulWidget {
+  mapPage createState()=> mapPage();
+}
+class mapPage extends State<IntroPage>{
+  late MapShapeSource _shapeSource;
 
+  @override
+  void initState() {
+    _shapeSource = MapShapeSource.asset('assets/world-map.json', shapeDataField: 'STATE_NAME');
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: const Text("INTROPAGE")),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text("Go to Home"),
-          onPressed: (){
-            Navigator.pushNamed(context, '/homepage');
-          },
+    return  Row(
+      children: [
+        Expanded(flex: 2,
+        child:
+        Container(
+          margin: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+
+            color: Colors.white,
+          border: Border.all(color: Colors.indigo),
+            borderRadius: BorderRadius.all(Radius.circular(20))
         ),
+        child: SfMaps(
+        layers: [MapShapeLayer(source: _shapeSource),],
       ),
+    ),
+    ),
+        Expanded(
+    flex: 1,
+    child:
+        Container(
+          margin: const EdgeInsets.all(20.0),
+          padding: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+
+              color: Colors.white,
+              border: Border.all(color: Colors.indigo),
+              borderRadius: BorderRadius.all(Radius.circular(20))
+          ),
+        ),
+    ),
+    ],
     );
   }
 }
